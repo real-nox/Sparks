533c1158777a2b9653a307125955b01931c205fa
Rayane <sirri.rayane.ma@gmail.com>
2026-04-03 18:26:23 +0100

    Startup kerfuffle system, will complete soon

--- src/systems/KerfuffleSystem.js
+++ src/systems/KerfuffleSystem.js
@@ -1,1 +1,10 @@
-//TO WORK ON SOON
+class Kerfuffle_Game {
+    constructor (guild_id, channel_id) {
+        this.guild_id = guild_id
+        this.channel_id = channel_id
+    }
+
+    Start_Game() {
+        
+    }
+}
